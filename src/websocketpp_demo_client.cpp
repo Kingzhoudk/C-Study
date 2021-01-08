6cc83f3055e43b198d70eeea9a7ba6c203da0ded
zhoudk <dingkun.zhou@uisee.com>
2021-01-08 17:41:04 +0800

    add websocketpp

--- src/websocketpp_demo_client.cpp
+++ src/websocketpp_demo_client.cpp
@@ -1,7 +1,16 @@
-#include "websocketpp/client.hpp"
+#include "websocketpp_demo_client.h"
+#include <atomic>
+#include <iostream>
+#include <map>
 
-
-
-int main(){
-    
+int main() {
+  std::string uri="ws://localhost:11234";
+  WebsocketClient client_ = WebsocketClient(uri);
+  client_.StartRunning();
+  while (true)
+  {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
+  
+  return 0;
 }
--- src/websocketpp_demo_server.cpp
+++ src/websocketpp_demo_server.cpp
@@ -1,38 +1,16 @@
-#include "websocketpp/config/asio_no_tls.hpp"
-#include "websocketpp/server.hpp"
+#include "websocketpp_demo_server.h"
 #include <atomic>
 #include <iostream>
-
-using websocketpp::lib::placeholders::_1;
-using websocketpp::lib::placeholders::_2;
-using websocketpp::lib::bind;
-
-// 取别名
-typedef websocketpp::server<websocketpp::config::asio> websocket_server;
-// pull out the type of messages sent by our config
-typedef websocket_server::message_ptr message_ptr;
-
-class WebsocketServer {
-  WebsocketServer(){
-    
-  }
-
-  // Define a callback to handle incoming messages
-  void on_message(websocket_server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
-    if (msg->get_payload() == "stop-listening") {
-        s->stop_listening();
-        return;
-    }
-
-    try {
-        s->send(hdl, msg->get_payload(), msg->get_opcode());
-    } catch (websocketpp::exception const & e) {
-        std::cout << "Echo failed because: "
-                  << "(" << e.what() << ")" << std::endl;
-    }
-  };
-};
+#include <map>
 
 int main() {
+  uint16_t port=11234;
+  WebsocketServer server_ = WebsocketServer(port);
+  server_.StartRunning();
+  while (true)
+  {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
+  
   return 0;
 }
